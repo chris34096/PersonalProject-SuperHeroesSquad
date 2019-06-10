000c555150932a1f40895a6d2767b8b375f75323
Christofer Perez <christoferperez@gmail.com>
2019-06-10 13:03:01 +0200

    adding some UI UX with CSS

---
+++ src/components/HeroList.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class HeroList extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Your Super Hero Squad</h4>
+        <ul className="list-group">
+          {this.props.heroes.map(hero => {
+            return (
+              <li key={hero.id} className="list-group-item">
+                <div className="list-item">{hero.name}</div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    heroes: state.heroes
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  null
+)(HeroList);
